4333eab1ecfeba54e7f7a9fae95b718859f433bc
MonkeyDluffySan <wwwwwahmedyounis@gmail.com>
2024-12-18 18:07:00 +0200

    TDD-handel-and-making-the-fizzbuzz-function

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-const fizzbuzz = () => {
-  // write your code here
-}
+const fizzbuzz = (number) => {
+  if (number % 3 == 0 && number % 5 == 0) {
+    return "FizzBuzz";
+  } else if (number % 3 == 0) {
+    return "Fizz";
+  } else if (number % 5 == 0) {
+    return "Buzz";
+  } else {
+    return number;
+  }
+};
 
 module.exports = fizzbuzz;
--- test.js
+++ test.js
@@ -1,5 +1,37 @@
-const fizzbuzz = require('./index.js');
+const fizzbuzz = require("./index.js");
 
-test('Testing Jest is working', () => {
-	expect(2 + 2).toBe(4);
+test("Testing Jest is working", () => {
+  expect(2 + 2).toBe(4);
+});
+
+describe("Testing fizzbuzz return value", () => {
+  test("Should return 8 when given 8", () => {
+    const actual = fizzbuzz(8);
+    const expected = 8;
+    expect(actual).toBe(expected);
+  });
+
+  test("Should return 2 when given 2", () => {
+    const actual = fizzbuzz(2);
+    const expected = 2;
+    expect(actual).toBe(expected);
+  });
+
+  test("Should return Buzz when givin 10", () => {
+    const actual = fizzbuzz(10);
+    const expected = "Buzz";
+    expect(actual).toBe(expected);
+  });
+
+  test("Should return FizzBuzz when givin 135", () => {
+    const actual = fizzbuzz(135);
+    const expected = "FizzBuzz";
+    expect(actual).toBe(expected);
+  });
+
+  test("Should return Fizz when givin 24", () => {
+    const actual = fizzbuzz(24);
+    const expected = "Fizz";
+    expect(actual).toBe(expected);
+  });
 });
